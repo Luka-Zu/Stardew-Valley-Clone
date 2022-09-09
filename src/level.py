39e78207e18707c153fc3adcbc72ac3d22e88ac8
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-09-09 11:19:42 +0400

    commit 1

--- src/level.py
+++ src/level.py
@@ -11,6 +11,8 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
 
     def run(self, dt):
-        self.display_surface.fill('black')
+        self.display_surface.fill('red')
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update()
+
+
